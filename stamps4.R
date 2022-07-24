c033c21c1c146636f5895bb8106c31d6f54ef69a
minsiksudo <minsik.kim@alumni.kaist.ac.kr>
2022-07-24 14:42:06 -0400

    Sunday afternoon, Modeling Microbiome Data1 - linear models

---
+++ stamps4.R
@@ -0,0 +1,16 @@
+#Lecture from Amy (Sun)
+
+
+
+#lm lab: 
+
+        download.file("https://raw.githubusercontent.com/statdivlab/stamps2022/main/Sunday-afternoon/labs/lm_lab/lm-ggplot-lab.R", "lm-ggplot-lab.R")
+#corncob lab: 
+        
+        download.file("https://raw.githubusercontent.com/statdivlab/stamps2022/main/Sunday-afternoon/labs/corncob_tutorial/corncob_tutorial.R", "corncob-lab.R")
+#rad emu lab: 
+        
+        download.file("https://raw.githubusercontent.com/statdivlab/stamps2022/main/Sunday-afternoon/labs/rademu_lab/rademu_lab.R", "rad-emu-lab.R")
+#diversity lab: 
+        
+        download.file("https://raw.githubusercontent.com/statdivlab/stamps2022/main/Sunday-afternoon/labs/diversity_lab/diversity-lab.R", "diversity-lab.R") 
